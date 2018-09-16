b7e2d46471921ff503ccfc1aad0f2a84fdc0843e
dfb93 <dfb93@cornell.edu>
2018-09-16 19:58:08 -0400

    averaging edits

--- Milestone1/Milestone1.ino
+++ Milestone1/Milestone1.ino
@@ -2,6 +2,18 @@
 Servo left;
 Servo right;
 int buttonPin = 8;
+
+/*Buffer variables*/
+int buffer_size = 4;
+int* leftmostSensorBuffer = new int [buffer_size];
+int* leftSensorBuffer = new int [buffer_size];
+int* rightSensorBuffer = new int [buffer_size];
+int* rightmostSensorBuffer = new int [buffer_size];
+int lmsbPointer = 0;
+int lsbPointer = 0;
+int rsbPointer = 0;
+int rmsbPointer = 0;
+
 void setup() {
   left.attach(5);
   right.attach(3);
@@ -22,30 +34,36 @@ void loop() {
   forwardAndLeft();
 }
 void forwardAndLeft(){
+  //setBufferDefault(leftmostSensorBuffer, 1); setBufferDefault(rightmostSensorBuffer, 1);
   while (readLeftmostSensor() == 1 || readRightmostSensor() == 1){
     trackLine();
   }
   turnLeft();
+  //setBufferDefault(leftSensorBuffer, 0); setBufferDefault(rightSensorBuffer, 0);
   while (readLeftSensor() == 0 && readRightSensor() == 0);
   while (readLeftSensor() == 1 || readRightSensor() == 1);
   stopMovement();
 }
 void forwardAndRight(){
+  //setBufferDefault(leftmostSensorBuffer, 1); setBufferDefault(rightmostSensorBuffer, 1);
   while (readLeftmostSensor() == 1 || readRightmostSensor() == 1){
     trackLine();
   }
   turnRight();
+  //setBufferDefault(leftSensorBuffer, 0); setBufferDefault(rightSensorBuffer, 0);
   while (readLeftSensor() == 0 && readRightSensor() == 0);
   while (readLeftSensor() == 1 || readRightSensor() == 1);
   stopMovement();
 }
 void forwardAndStop(){
+  //setBufferDefault(leftmostSensorBuffer, 1); setBufferDefault(rightmostSensorBuffer, 1);
   while (readLeftmostSensor() == 1 || readRightmostSensor() == 1){
     trackLine();
   }
   stopMovement();
 }
 void trackLine(){
+    //setBufferDefault(leftSensorBuffer, 0); setBufferDefault(rightSensorBuffer, 0);
     if (readLeftSensor() == 0 && readRightSensor() == 0)
       forward();
     else if (readLeftSensor() == 0 && readRightSensor() == 1){
@@ -61,24 +79,32 @@ void trackLine(){
 /* returns 0 if white detected, 1 if black */
 int readLeftSensor(){
   int val = analogRead(0);
+  //update_buffer(leftSensorBuffer, lsbPointer, val);
+  //val = average_buffer(leftSensorBuffer);
   Serial.print(val);
   Serial.print(" ");
   return val > 800? 1:0;
   }
 int readRightSensor(){
   int val = analogRead(1);
+  //update_buffer(rightSensorBuffer, rsbPointer, val);
+  //val = average_buffer(leftSensorBuffer);
   Serial.print(val);
   Serial.print(" ");
   return val > 800? 1:0;
   }
 int readLeftmostSensor(){
   int val = analogRead(2);
+  //update_buffer(leftmostSensorBuffer, lmsbPointer, val);
+  //val = average_buffer(leftSensorBuffer);
   Serial.print(val);
   Serial.print(" ");
   return val > 600? 1:0;
   }
 int readRightmostSensor(){
   int val = analogRead(3);
+  //update_buffer(rightmostSensorBuffer, rmsbPointer, val);
+  //val = average_buffer(leftSensorBuffer);
   Serial.print(val);
   Serial.print("\n");
   return val > 600? 1:0;
@@ -111,3 +137,29 @@ void stopMovement(){
   writeLeft(90);
   writeRight(90);
   }
+
+/* filter functions */
+void update_buffer(int buffer[], int &p, int val) {
+  buffer[p] = val;
+  p = (p = buffer_size-1) ? 0 : (p+1);
+}
+
+void setBufferDefault(int buffer[], int val) {
+  for(int i = 0; i < buffer_size; i++) {
+      buffer[i] = val*1023;
+  }
+}
+
+int average_buffer(int buffer[]) {
+  int sum = 0;
+  for(int i = 0; i < buffer_size; i++) {
+      sum +=buffer[i];
+  }
+  return sum/buffer_size;
+}
+
+
+
+
+
+
