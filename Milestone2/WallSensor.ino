665c3df250053ca93fd894a3e8d0242e287c0b2e
dfb93 <dfb93@cornell.edu>
2018-10-10 12:48:05 -0400

    moved wall sensor code to arduino proj

--- Milestone2/WallSensor.ino
+++ Milestone2/WallSensor.ino
@@ -1,18 +1,22 @@
+//Depricated: Currently in Milestone2.ino
 //TODO: wall sensor logic
 
 /* read forward-facing sensor
  * Returns 1 for wall detected and 0 for no wall detected
  */
+ /*
 int readForwardWallSensor() {
   return 0;
-}
+}*/
 
 /* read right-facing sensor. 
  *  Returns 1 for wall detected and 0 for no wall detected
  */
+
+ /*
 int readRightWallSensor() {
   return 0;
-}
+}*/
 
 
 /* At an intersection, this function determines which direction
@@ -23,8 +27,12 @@ int readRightWallSensor() {
  *  
  *  Later: record and send detected wall data
  */
+
+ /*
 int detectWalls() {
   return 2;
   
 }
+*/
+
 
