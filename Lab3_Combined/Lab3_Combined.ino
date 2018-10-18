a0fb9608647213dcf75ac8a3a898ef01a3156d86
dfb93 <dfb93@cornell.edu>
2018-10-17 20:13:15 -0400

    lab 3 combined

---
+++ Lab3_Combined/Lab3_Combined.ino
@@ -0,0 +1,259 @@
+#include <Servo.h>
+Servo left;
+Servo right;
+int buttonPin = 7;
+int rightWallSensor = A5;
+int rightWallLED = 6;
+int frontWallSensor = A4;
+int frontWallLED = 8;
+int mux0=2;
+int mux1=3;
+int muxRead = A3;
+int muxReadDelay = 6; //ms delay before reading from the mux to handle some switching issues
+
+bool fft_detect = false;
+void setup() {
+  left.attach(5);//left servo pin 5
+  right.attach(4);//right servo pin 4
+  stopMovement();
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+   pinMode(rightWallSensor, INPUT);
+  pinMode(frontWallSensor, INPUT);
+  pinMode(rightWallLED, OUTPUT);
+  pinMode(frontWallLED, OUTPUT);
+  pinMode(mux0, OUTPUT);
+  pinMode(mux1, OUTPUT);
+  Serial.println("Wait for button");
+  while(digitalRead(buttonPin)==LOW);
+  //Serial.println("Button Pressed");
+  fft_setup();//ADDED
+  Serial.println("FFT Setup Complete");
+  
+}
+
+void loop() {
+//  forwardAndLeft();
+//  
+//  forwardAndRight();
+//
+//  forwardAndRight();
+// 
+//  forwardAndRight();
+// 
+//  forwardAndRight();
+//  
+//  forwardAndLeft();
+// 
+//  forwardAndLeft();
+//  
+//  forwardAndLeft();
+// 
+Serial.println("Loop");
+ 
+   int hasRightWall = readRightWallSensor();
+   int hasFrontWall = readForwardWallSensor();
+    /*
+   while(true) {
+    fft_analyze();
+    Serial.println(fft_detect);
+    
+   }*/
+   if (hasRightWall==1&&hasFrontWall==0) {
+    forwardAndStop();
+   }
+   else if (hasRightWall==1&&hasFrontWall==1){
+    turnLeft();
+    finishTurn();
+   }
+   else if (hasRightWall==0){
+    turnRight();
+    finishTurn();
+    forwardAndStop();
+   }
+// troubleshooting code block:
+//   int leftmost = readLeftmostSensor();
+//   int left = readLeftSensor();
+//   int right = readRightSensor();
+//   int rightmost = readRightmostSensor();
+//   Serial.print("|");
+//   int hasRightWall = readRightWallSensor(); 
+//   int hasFrontWall = readForwardWallSensor();
+//   Serial.print(hasRightWall);
+//   Serial.print(" ");
+//   Serial.print(hasFrontWall);
+//   Serial.println();
+}
+
+void finishTurn(){
+  while (readLeftSensor() == 0 || readRightSensor() == 0);//robot exits line (breaks loop when both sensors are dark)
+  while (readLeftSensor() == 1 && readRightSensor() == 1);//robot enters line again (both line sensors light)
+  stopMovement();
+}
+void forwardUntilOffIntersection() {
+  while (readLeftmostSensor() == 0 && readRightmostSensor() == 0){
+    trackLine();
+  }
+}
+
+void forwardAndStop(){
+    int i = 0;
+    while (readLeftmostSensor() == 1 || readRightmostSensor() == 1){
+      //Serial.println("BEGIN");
+      trackLine();
+      //perform FFT every __ number of cycles
+      if (i == 5) {
+        Serial.println("Running fft");
+        /*
+        stopMovement();
+        while(true) {
+          fft_analyze();
+          Serial.println(fft_detect);
+        }*/
+        fft_analyze();
+        while (fft_detect/**/) {
+          stopMovement();
+          fft_analyze();
+        } //Added
+        i = 0;
+      }
+      else { i++; }
+    }
+  stopMovement();
+}
+
+void forwardAndLeft(){
+  while (readLeftmostSensor() == 1 || readRightmostSensor() == 1){ //This statement detects an intersection (both outer sensors go light to break loop)
+    trackLine();
+  }
+  turnLeft();
+  while (readLeftSensor() == 0 && readRightSensor() == 0);//robot exits line (both line sensors dark)
+  while (readLeftSensor() == 1 || readRightSensor() == 1);//robot enters line again (both line sensors light)
+  stopMovement();
+}
+
+void forwardAndRight(){
+  while (readLeftmostSensor() == 1 || readRightmostSensor() == 1){
+    trackLine();
+  }
+  turnRight();
+  while (readLeftSensor() == 0 && readRightSensor() == 0);
+  while (readLeftSensor() == 1 || readRightSensor() == 1);
+  stopMovement();
+}
+
+void trackLine(){
+    if (readLeftSensor() == 0 && readRightSensor() == 0)
+      forward();
+    else if (readLeftSensor() == 0 && readRightSensor() == 1){
+      writeRight(180);
+      writeLeft(90);
+    }
+    else if (readLeftSensor() == 1 && readRightSensor() == 0){
+      writeLeft(180);
+      writeRight(90);
+    }else
+      backward();
+  }
+
+/* read forward-facing sensor
+ * Returns 1 for wall detected and 0 for no wall detected
+ */
+int readForwardWallSensor() {
+  int val = analogRead(frontWallSensor);
+  //Serial.print(val);
+  //Serial.print(" ");
+  if (val>180){
+    digitalWrite(frontWallLED, HIGH);
+    return 1;
+  }
+  else{
+    digitalWrite(frontWallLED, LOW);
+    return 0;
+  }
+}
+
+/* read right-facing sensor. 
+ *  Returns 1 for wall detected and 0 for no wall detected
+ */
+int readRightWallSensor() {
+  
+  int val = analogRead(rightWallSensor);
+  //Serial.print(val);
+  //Serial.print(" ");
+  if (val>180){
+    digitalWrite(rightWallLED, HIGH);
+    return 1;
+  }
+  else{
+    digitalWrite(rightWallLED, LOW);
+    return 0;
+  }
+}
+  
+/* returns 0 if white detected, 1 if black */
+int readLeftSensor(){
+  digitalWrite(mux0, LOW);
+  digitalWrite(mux1, LOW);
+  delay(muxReadDelay);
+  int val = analogRead(muxRead);
+  //Serial.print(val);
+  //Serial.print(" ");
+  return val > 357? 1:0;
+  }
+int readRightSensor(){
+  digitalWrite(mux0, HIGH);
+  digitalWrite(mux1, LOW);
+  delay(muxReadDelay);
+  int val = analogRead(muxRead);
+  //Serial.print(val);
+  //Serial.print(" ");
+  return val > 297? 1:0;
+  }
+int readLeftmostSensor(){
+  digitalWrite(mux0, HIGH);
+  digitalWrite(mux1, HIGH);
+  delay(muxReadDelay);
+  int val = analogRead(muxRead);
+  //Serial.print(val);
+  //Serial.print(" ");
+  return val > 700? 1:0;
+  }
+int readRightmostSensor(){
+  digitalWrite(mux0, LOW);
+  digitalWrite(mux1, HIGH);
+  delay(muxReadDelay);
+  int val = analogRead(muxRead);
+  //Serial.print(val);
+//  Serial.print("\n");
+  return val > 570? 1:0;
+  }
+/* writing 180 to both functions would go forward, 0 would go backward*/
+void writeLeft(int speed){
+    left.write(speed);
+  }
+void writeRight(int speed){
+    right.write(180-speed);
+  }
+
+void backward(){
+  writeLeft(0);
+  writeRight(0);
+  }
+void forward(){
+  writeLeft(180);
+  writeRight(180);
+  }
+void turnLeft(){
+  writeLeft(0);
+  writeRight(180);
+  }
+void turnRight(){
+  writeLeft(180);
+  writeRight(0);
+  }
+void stopMovement(){
+  writeLeft(90);
+  writeRight(90);
+  }
+
--- Lab3_Combined/Signal_Processing.ino
+++ Lab3_Combined/Signal_Processing.ino
@@ -1,10 +1,6 @@
 /*
-fft_adc_serial.pde
-guest openmusiclabs.com 7.7.14
-example sketch for testing the fft library.
-it takes in data on ADC0 (Analog0) and processes them
-with the fft. the data is sent out over the serial
-port at 115.2kb.
+FFT analysis code for Audio and IR signal analysis
+Based off example sketch fft_adc_serial provided by the FFT library
 */
 
 #define LOG_OUT 1 // use the log output function
@@ -19,15 +15,14 @@ int threshold = 140;//the monitored signal bin amplitude threshold for detection
 int start_count = 0;// counter for keeping track of back-to-back detections
 int count_max = 5;// the number of back-to-back detections for a confirmed detection
 
-//TODO: add any IR local variables here
-
 int mux_pin = 2; //TODO: change this port to the desired mux pin. LOW = Audio, HIGH = IR
 
-void setup() {
-  Serial.begin(9600); // use the serial port
-  Serial.println("Waiting for 660 Hz");
-  TIMSK0 = 0; // turn off timer0 for lower jitter
-  ADCSRA = 0xe7; // set the adc to free running mode (change to 0xe7 for default resolution?)
+int OLD_ADCSRA;
+
+//The setup FFT function
+void fft_setup() {
+  //TIMSK0 = 0; // turn off timer0 for lower jitter
+  //ADCSRA = 0xe7; // set the adc to free running mode (change to 0xe7 for default resolution?)
   ADMUX = 0x40; // use adc0
   DIDR0 = 0x01; // turn off the digital input for adc0
 
@@ -35,14 +30,20 @@ void setup() {
   digitalWrite(mux_pin, LOW); 
 }
 
-void loop() {
-  while(1) { // reduces jitter
+//Performs a full FFT analysis. ~ 256 samples/sample rate (~26 ms Audio, ~3 ms IR) 
+//return true for valid detection. false for no detection
+void fft_analyze() {
+    fft_setup();
+    OLD_ADCSRA = ADCSRA;
     cli();  // UDRE interrupt slows this way down on arduino1.0
     for (int i = 0 ; i < 256 ; i += 2) { // save 256 samples
       while(!(ADCSRA & 0x10)); // wait for adc to be ready
       
-      if (!has_started) ADCSRA = 0xf7; // restart adc for audio
-      else  ADCSRA = 0xf4; //TODO: set for IR prescale factor
+      if (!has_started) ADCSRA = 0xf7; // restart adc for audio (128 prescale factor) [9600 Hz sample rate]
+      else  {
+        ADCSRA = 0xe4;
+        ADCSRA = 0xf4; //IR prescale factor (16) [~76900 Sample Rate]
+      }
       
       byte m = ADCL; // fetch adc data
       byte j = ADCH;
@@ -73,32 +74,40 @@ void loop() {
           has_started = true;//start the Robot and IR analysis
           digitalWrite(mux_pin, HIGH);//Set mux to IR input
           start_count = 0;
+          fft_detect = true;
        }
+        else  fft_detect = false;
       }
-      else { start_count = (start_count != 0) ? start_count - 1 : 0;}
+      else { 
+       start_count = (start_count != 0) ? start_count - 1 : 0;
+       fft_detect = false;
+       }
     }
     //IR Analysis
     else {
-      if (IR_initilized == false) {ADCSRA = 0xe4;
+      if (IR_initilized == false) {//ADCSRA = 0xe4;
       IR_initilized = true;
       }
       else{
-        //Serial.println(fft_log_out[42]);
         //Serial.println(fft_log_out[21]);
         if (fft_log_out[11] > 55  ){
             Serial.println("Robot Detected!!!");
+            fft_detect = true;
         }
         else{
-          //Serial.println("No Robot Detected");
+           Serial.println("No Robot Detected");
            start_count = (start_count != 0) ? start_count - 1 : 0;
-        }}
+           fft_detect = false;
+        } 
+      }
     }
-
+   
+    ADCSRA = OLD_ADCSRA;
     /*
     Serial.println("start");
     for (byte i = 0 ; i < FFT_N/2 ; i++) { 
       Serial.println(fft_log_out[i]); // send out the data
     }*/
     
-  }
+  
 }
