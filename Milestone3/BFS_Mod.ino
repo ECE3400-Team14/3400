cb9a8a11f9ac29944a704e7ec23f7a0e53034fca
dfb93 <dfb93@cornell.edu>
2018-11-12 13:22:59 -0500

    refactorized bfs with node memory storage

--- Milestone3/BFS_Mod.ino
+++ Milestone3/BFS_Mod.ino
@@ -1,3 +1,13 @@
+/* TODO: For Dijkstra: 
+ *  -add weights to nodes
+ *  -allow parent swapping
+ *    -try going to node
+ *    -see that a node there aready exists
+ *    -compare path lengths from current path and the one of the node already there
+ *    -if new path is lower, replace other node's parent with this node. 
+ * 
+ */
+
 struct Node {
   byte position;//x,y coordinates of the node
   char move;//move used to get here
@@ -5,16 +15,55 @@ struct Node {
   struct Node* parent;//the parent of this node
 };
 
-QueueArray<Node>* nodeQueue;
+QueueArray<Node*>* nodeQueue;//array of node (addresses)
 
 void initNodeQueue() {
-  nodeQueue = new QueueArray<Node>;
+  nodeQueue = new QueueArray<Node*>;
 }
 
 //call when done with the tree (after generating path to next node)
 void clearNodeQueue() {
  delete nodeQueue;
 }
+
+const int v_max_size = mazeSize;//(TODO: Calculate theoretical max size)
+struct Node visitedNodes[v_max_size*2];//array for storing node data
+byte v_index = 0;//index where to enter new nodes
+
+/* 
+ * Reset list of visited nodes by algorithm
+ */
+void initVisitedNodes() {
+  v_index = 0;
+}
+
+/*
+ * add a new node 
+ */
+void addVisitedNode(struct Node v) {
+  if(v_index == v_max_size) {
+    //???? FAIL!
+  }
+  else {
+    visitedNodes[v_index] = v;
+    v_index++;
+  }
+}
+
+struct Node* getTopAddr() {
+  return &visitedNodes[v_index];
+}
+
+/*
+ * check if a node with position [pos] is already in the visited list
+ */
+bool visitedContains(byte pos) {
+  for(int i = 0; i < v_index; i++) {
+    if (visitedNodes[i].position == pos) return true;
+  }
+  return false;
+}
+
 
 //struct Queue graph_visited;//a queue of visited coordinates in nodeQueue;
 
@@ -35,70 +84,77 @@ void clearNodeQueue() {
 void bfs_mod(int start_pos) {
 
   initNodeQueue();//initialize the queue (memory leaks?)
-  initialize_bfs();//initialize queue to store coordinates visited by the algorithm
+  //initialize_bfs();//initialize queue to store coordinates visited by the algorithm
+  initVisitedNodes();//reset visited list
   struct Node start;
   start.position = start_pos;
   start.move = 's';
   start.parent = NULL;
   start.orientation = orientation;
-  nodeQueue->push(start);
+  addVisitedNode(start);
+  nodeQueue->push(getTopAddr());
   
-  bfs_append_visited(start_pos);
   while (!nodeQueue->isEmpty()) {
-    struct Node v = nodeQueue->pop();
-    int pos = v.position;
+    struct Node* v = nodeQueue->pop();
+    int pos = v->position;
     //if this node hasn't been explored yet, return path to node
     if(!isExplored(getX(pos),getY(pos) ) ){
-      constructMovements(&v);
+      constructMovements(v);
       return;
     }
     //if the node is not in the frontier, keep looking
     else 
-      if(!bfs_has_visited(pos)) {
-        bfs_append_visited(pos);
-      }
+//      if(!bfs_has_visited(pos)) {
+//        bfs_append_visited(pos);
+//      }
         //if forward is an option
-        if(canGoForward(x,y,v.orientation)) {
-          int next_orientation = v.orientation;
+        if(canGoForward(getX(pos),getY(pos),v->orientation)) {
+          int next_orientation = v->orientation;
           int next_pos = nextCoor(getX(pos),getY(pos),next_orientation);
-          if(!bfs_has_visited(next_pos)) {//if next coordinate is not visited
+          if(!visitedContains(next_pos)) {//if next coordinate is not visited //!bfs_has_visited(next_pos)
             //add Node with next coordinate and 'f'
             struct Node next_v;
             next_v.position = next_pos;//update node position
             next_v.move = 'f';
             next_v.orientation = next_orientation;
-            next_v.parent = &v;
-            nodeQueue->push(next_v);
+            next_v.parent = v;
+            //put next_v in visited array
+            addVisitedNode(next_v);//store this node in memory
+            nodeQueue->push(getTopAddr());
           }
           
         }
         //if left is an option (turn left with not wall)
-        if(canGoLeft(x,y,v.orientation)) {
-          int next_orientation = (v.orientation == 0) ? 3 : v.orientation - 1;
+        if(canGoLeft(getX(pos),getY(pos),v->orientation)) {
+          int next_orientation = (v->orientation == 0) ? 3 : v->orientation - 1;
           int next_pos = nextCoor(getX(pos),getY(pos),next_orientation);
-          if(!bfs_has_visited(pos)) {//if next coordinate is not visited
+          if(!visitedContains(next_pos)) {//if next coordinate is not visited
             //add Node with this coordinate and 'l'
             struct Node next_v;
             next_v.position = pos;//keep the same position
             next_v.move = 'l';
             next_v.orientation = next_orientation;
-            next_v.parent = &v;
-            nodeQueue->push(next_v);
+            next_v.parent = v;
+            //nodeQueue->push(next_v);
+            addVisitedNode(next_v);//store this node in memory
+            nodeQueue->push(getTopAddr());
           }
         }
 
         //if right is an option (turn right with not wall)
-        if(canGoRight(x,y,v.orientation)) {
-          int next_orientation = (v.orientation == 3) ? 0 : v.orientation + 1;
+        if(canGoRight(getX(pos),getY(pos),v->orientation)) {
+          int next_orientation = (v->orientation == 3) ? 0 : v->orientation + 1;
           int next_pos = nextCoor(getX(pos),getY(pos),next_orientation);
-          if(!bfs_has_visited(pos)) {//if next coordinate is not visited
+          if(!visitedContains(next_pos)) {//if next coordinate is not visited
             //add Node with this coordinate and 'r'
             struct Node next_v;
             next_v.position = pos;//keep the same position
             next_v.move = 'r';
             next_v.orientation = next_orientation;
-            next_v.parent = &v;
-            nodeQueue->push(next_v);
+            next_v.parent = v;
+            //nodeQueue->push(next_v);
+            addVisitedNode(next_v);//store this node in memory
+            nodeQueue->push(getTopAddr());
           }
         }
 
