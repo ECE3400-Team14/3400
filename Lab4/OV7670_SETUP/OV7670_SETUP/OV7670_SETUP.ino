168ea8caaff9b18889fb25786949a03f8fde8318
dfb93 <dfb93@cornell.edu>
2018-11-18 15:50:08 -0500

    added some color regs

--- Lab4/OV7670_SETUP/OV7670_SETUP/OV7670_SETUP.ino
+++ Lab4/OV7670_SETUP/OV7670_SETUP/OV7670_SETUP.ino
@@ -17,12 +17,16 @@ void setup() {
   OV7670_write_register(0x0C, 0x8); //COM3 enable scaling
   OV7670_write_register(0x11, 0xC0);//use external clock
   OV7670_write_register(0x12, 0xC);//set camera pixel format and enable color bar test with 0xE disable with 0xC
-  OV7670_write_register(0x14, 0x01);//automated gain ceiling of 2x
+  OV7670_write_register(0x14, 0x02);//automated gain ceiling of 2x
   OV7670_write_register(0x40, 0xD0);//COM15 set for RGB 565 11010000 (208) D0
   OV7670_write_register(0x1E, 0x30); //mirror and flip
   OV7670_write_register(0x8C, 0x2); //RGB444
   OV7670_write_register(0x42, 0x0);//COM17 enable DSP color bar
 
+  //OV7670_write_register(0x01, 0x90);//adjust blue gain
+  //OV7670_write_register(0xBE, 0x90);//black level comp (blue)
+  //OV7670_write_register(0x55, 0x30);//brightness?
+  
   read_key_registers();
   set_color_matrix();
   Serial.println("The arduino has set the color matrix");
@@ -109,7 +113,7 @@ void set_color_matrix(){
     OV7670_write_register(0x52, 0x22);
     OV7670_write_register(0x53, 0x5e);
     OV7670_write_register(0x54, 0x80);
-    OV7670_write_register(0x56, 0x40);
+    OV7670_write_register(0x56, 0x40);//contrast (0x40)
     OV7670_write_register(0x58, 0x9e);
     OV7670_write_register(0x59, 0x88);
     OV7670_write_register(0x5a, 0x88);
